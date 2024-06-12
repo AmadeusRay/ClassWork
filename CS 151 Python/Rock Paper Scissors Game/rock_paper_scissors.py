3086b15ff7025136321b59e7318ee9ab7b30dabe
amadeusRay <arashvasheghani@gmail.com>
2024-06-11 20:26:42 -0700

    Rock Paper Scissors game Complete
    Moved from incomplete to Python class folder

--- CS 151 Python/Rock Paper Scissors Game/rock_paper_scissors.py
+++ CS 151 Python/Rock Paper Scissors Game/rock_paper_scissors.py
@@ -1,20 +1,38 @@
+#meta info
+
+#author: Arash Vasheghani
+#date: June 10, 2024
+#version: 1
+# Is this a part of another program? No
+
+#Python Paper Rock Scissors
+
+#Pseudocode
+#Create and return computer choice through a list and randomly choosing from the list (rock paper scissors) for for get_
+# computer_choice
+#Create and return user choice by asking "Paper, rock, or scissors: " for def get_user_choice
+#Create and return who is the winner. Use if statements for the logic of what defeats what in paper rock scissors
+# (assuming user will be winner in each if statement) and assigning the winner to user. Else is computer.
+
 import random
+
 
 def get_computer_choice():
     # Randomly selected between "rock", "paper", and "scissors" for the computer's choice
-    choiceList = ["rock", "paper", "scissors"]
-    return random.choice(choiceList)
+    choice_list = ["rock", "paper", "scissors"]  #the list we will randomly choose from
+    return random.choice(choice_list)  #returns random choice from the choiceList
 
 
 def get_user_choice():
     # Get the user's choice and convert it to lowercase
-    userChoice = input("Paper, rock, or scissors: ")  # Asks for user input for the second number and
-    return userChoice
+    user_choice: str = input("Paper, rock, or scissors: ")  # Asks for user input
+    return user_choice  # returns user input
+
 
 def determine_winner(user_choice, computer_choice):
     # Determine the winner based on the game rules
-    choices = [user_choice + computer_choice]
 
+    # logic for who is the winner of paper rock scissors and assigning it to winner
     if user_choice == computer_choice:
         winner = "TIE"
     elif user_choice == "rock" and computer_choice == "scissors":
@@ -26,6 +44,7 @@ def determine_winner(user_choice, computer_choice):
     else:
         winner = "Computer"
     return winner
+
 
 def main():
     print("Welcome to Rock Paper Scissors!")
@@ -51,6 +70,7 @@ def main():
         # Determine and print the results
         result = determine_winner(user_choice, computer_choice)
         print(result)
+
 
 if __name__ == "__main__":
     main()
